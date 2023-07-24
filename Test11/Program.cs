9dc32bf5e4a92152b3d18aa981d75858b72758b8
KaraRi <kirarichi51@gmail.com>
2023-07-24 16:53:07 +0200

    Поиск среднего из трёх.

---
+++ Test11/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 10: Напишите программу, 
+// которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+metke: Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 0)
+{
+    number *= -1;
+}
+if(number < 100)
+{
+    Console.WriteLine("У вас проблемы?");
+    Console.WriteLine("Попробуйте снова =)");
+    goto metke;
+}
+if(number > 999)
+{
+    Console.WriteLine("У вас проблемы?");
+    Console.WriteLine("Попробуйте снова =)");
+    goto metke;
+}
+int secondDigit = (number / 10) % 10;
+
+Console.WriteLine($"Второе число трёхзначного числа:{secondDigit}");
