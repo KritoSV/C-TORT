3de4a19941cbe83ed688329529b413fc41bebe33
KaraRi <kirarichi51@gmail.com>
2023-07-27 22:27:47 +0200

    Таблица ккубов до N.

---
+++ Test19/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 23
+// Напишите программу, которая принимает 
+// на вход число (N) и выдаёт таблицу 
+// кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+{
+    Console.WriteLine("Некорректные данные");
+    return;
+}
+SquareTable(number);
+
+void SquareTable(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        int square = i * i * i;
+        Console.WriteLine($"{i, 3}     {square, 5}");
+    }
+} 
