1d907cead4b2ecbe44a025959cc7bd55416802a1
KaraRi <kirarichi51@gmail.com>
2023-07-31 10:21:45 +0200

    Произведение чисел.

---
+++ Test22/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите натуральное число: ");
+double number = Convert.ToInt32(Console.ReadLine());
+
+double factorial = Factorial(number);
+Console.WriteLine($"Количество цифр в числе {number} -> {factorial}");
+
+double Factorial(double num)
+{
+    double fac = 1;
+    for (double i = 1; i < num; i++)
+    {
+        checked
+        {
+            fac *= i; // fac = fac * i
+        }
+    }
+    return fac;
+}
