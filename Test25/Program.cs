1e0749933ce87d7d0c26eb9455d28bd8c6e9a2e4
KaraRi <kirarichi51@gmail.com>
2023-07-31 14:30:29 +0200

    Сумма цифр числа.

---
+++ Test25/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 27: 
+// Напишите программу, которая 
+// принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+metka: Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+{
+    Console.WriteLine("Пожалуйста схдите НА ХУЙ и принесите натуральное число.");
+    goto metka;
+}
+
+int sumNumber = SumNumber(number);
+Console.WriteLine($"Сумма всех цифр числа {sumNumber}");
+
+int SumNumber(int num)
+{
+    int result = 0;
+    while (num != 0)
+    {
+        result = result + num % 10;
+        num = num / 10;
+    }
+    return result;
+}
