0b165433081e7a06389f924bc75e91e2e4f8c4fd
KaraRi <kirarichi51@gmail.com>
2023-07-31 14:46:18 +0200

    Массив с псевдочислами.

---
+++ Test26/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: 
+// Напишите программу, 
+// которая задаёт массив 
+// из 8 элементов и 
+// выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] array = new int[8];
+
+FillArr(array);
+PrintArr(array);
+
+void FillArr(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-99, 100);
+    }
+}
+
+void PrintArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
