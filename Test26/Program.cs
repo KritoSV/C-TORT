8bbdb0f0d28caa3a3b341f2e04a9ef80d4a7eaab
KaraRi <kirarichi51@gmail.com>
2023-07-31 14:50:20 +0200

    Массив с псевдочислами 1.

--- Test26/Program.cs
+++ Test26/Program.cs
@@ -15,7 +15,7 @@ void FillArr(int[] arr)
     Random rnd = new Random();
     for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = new Random().Next(-99, 100);
+        arr[i] = rnd.Next(-99, 100);
     }
 }
 
