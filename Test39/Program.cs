2e2f1690ea9433c15abca42a9f2c66aaa2fa9e3f
KaraRi <kirarichi51@gmail.com>
2023-08-27 15:49:11 +0200

    Математика для Сынусика.

---
+++ Test39/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Clear();
+
+
+
+Console.WriteLine("Здравствуйте пользователь! Давайте поиграем в математику.");
+
+
+int min = 1;
+int max = 20;
+
+
+int a = new Random().Next(min, max);
+int b = new Random().Next(min, max);
+int sum = 0;
+
+
+Console.Write($"Напишите сумму {a} + {b} -> ");
+int userNum = int.Parse(Console.ReadLine()!);
+
+sum = SumNumbers(a, b);
+UserSumNumber(userNum, sum);
+
+
+void UserSumNumber(int un, int s)
+{
+    if (un == s)
+    {
+        Console.WriteLine($"Молодец, {userNum} это правильный ответ.");
+    }
+    else
+    {
+        Console.WriteLine($"{userNum} это не правильный ответ. правильный ответ -> {sum}");
+    }
+}
+
+int SumNumbers(int x, int y)
+{
+    int summa = x + y;
+    return summa;
+}
+
