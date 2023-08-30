8318f7c33028574dba1f54597c7e044a086babd8
KaraRi <kirarichi51@gmail.com>
2023-08-30 21:25:12 +0200

    Заготовка.

---
+++ Test43/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив 
+// из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно 
+// выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// Результат:
+// 66(0,0,0) 25(0,1,0) 27(0,0,1) 90(0,1,1)
+// 34(1,0,0) 41(1,1,0) 26(1,0,1) 55(1,1,1)
+
+
+int minimal = 1; int maximal = 5;
+int[, ,] matrix1 = ArrayMass(2, 2, minimal, maximal);
+
+
+
+
+int[, ,] ArrayMass(int m, int n, int min, int max)
+{
+    int[, ,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = new Random().Next(min, max + 1);
+                Console.Write($"{array[i, j, k],2} ");
+            }
+            Console.WriteLine();
+        }
+    }
+    return array;
+}
